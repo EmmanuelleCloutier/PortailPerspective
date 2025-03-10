050ba225df01dc0477acd790a85d863a10a02948
EmmanuelleCloutier <darkforest1045@gmail.com>
2025-03-10 11:03:33 -0400

    added c++

---
+++ Source/Portail/Private/Portails.cpp
@@ -0,0 +1,26 @@
+
+
+#include "Portails.h"
+
+
+APortails::APortails()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+
+void APortails::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void APortails::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

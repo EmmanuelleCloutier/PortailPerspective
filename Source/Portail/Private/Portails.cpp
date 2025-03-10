61bf02c505b65dd3643d7a6d571f11c797a5694e
EmmanuelleCloutier <darkforest1045@gmail.com>
2025-03-10 11:53:19 -0400

    logique portail c++

--- Source/Portail/Private/Portails.cpp
+++ Source/Portail/Private/Portails.cpp
@@ -1,26 +1,51 @@
-
-
 #include "Portails.h"
-
+#include "Components/BoxComponent.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "GameFramework/Actor.h"
 
 APortails::APortails()
 {
- 	
 	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-
-void APortails::BeginPlay()
-{
-	Super::BeginPlay();
 	
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	RootComponent = SceneRoot;
+	
+	PortalCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("PortalCollision"));
+	PortalCollision->SetupAttachment(SceneRoot);
+	PortalCollision->SetBoxExtent(FVector(50.0f, 50.0f, 100.0f));
+	
+	PortalMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PortalMesh"));
+	PortalMesh->SetupAttachment(SceneRoot);
+	
+	CaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>(TEXT("CaptureComponent"));
+	CaptureComponent->SetupAttachment(SceneRoot);
+	
+	PortalCollision->OnComponentBeginOverlap.AddDynamic(this, &APortails::OnOverlapBegin);
 }
-
 
 void APortails::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void APortails::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                               UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && OtherActor != this && LinkedPortal)
+	{
+		TeleportActor(OtherActor);
+	}
+}
+
+
+
+void APortails::TeleportActor(AActor* Actor)
+{
+	if (!LinkedPortal) return;
+
+	FVector NewLocation = LinkedPortal->GetActorLocation();
+	FRotator NewRotation = LinkedPortal->GetActorRotation();
+
+	Actor->SetActorLocation(NewLocation);
+	Actor->SetActorRotation(NewRotation);
+}
--- Source/Portail/Public/Portails.h
+++ Source/Portail/Public/Portails.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -10,17 +9,36 @@ UCLASS()
 class PORTAIL_API APortails : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	APortails();
+
+public:
+	APortails(); 
+
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+public:    
 	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USceneComponent* SceneRoot;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UBoxComponent* PortalCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* PortalMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USceneCaptureComponent2D* CaptureComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	APortails* LinkedPortal;
+	
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+						bool bFromSweep, const FHitResult & SweepResult);
+	
+	void TeleportActor(AActor* Actor);
 };
